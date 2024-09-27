76f4ade1fa023af7f9b93cbfd97512331e3ab361
max 96 <119216428+max96git@users.noreply.github.com>
2024-09-27 22:29:23 +0300

    Create auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+const JWT_SECRET = 'your_jwt_secret'; // Make sure to use an environment variable in production
+
+router.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).send('User created');
+});
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign({ id: user._id }, JWT_SECRET);
+        res.json({ token });
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
+});
+
+module.exports = router;
