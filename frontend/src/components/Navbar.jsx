cf96117494bf383a55979267609aa141359db433
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-28 17:49:36 +0300

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,24 @@
+// src/components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './Navbar.css'; // Import your CSS file for styling
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img
-          src="https://tr.rbxcdn.com/2d3b1ae7c0a4d640fca2510826d7d0b4/420/420/Hat/Png"
-          alt="Hexanoid Logo"
-          className="logo"
-        />
-        <span className="navbar-title">Hexanoid</span>
-      </div>
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/sell-limiteds">Sell Limiteds</Link>
-        <Link to="/sell-accounts">Sell Accounts</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src="https://tr.rbxcdn.com/2d3b1ae7c0a4d640fca2510826d7d0b4/420/420/Hat/Png" alt="Hexanoid Logo" />
+                <span>Hexanoid</span>
+            </div>
+            <div className="navbar-links">
+                <Link to="/">Home</Link>
+                <Link to="/sell-limiteds">Sell Limiteds</Link>
+                <Link to="/sell-accounts">Sell Accounts</Link>
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Signup</Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
