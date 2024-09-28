85b78c3ec517b12e559fc309bd4413013c97a94e
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-28 18:16:38 +0300

    Create api.js

---
+++ frontend/src/api/api.js
@@ -0,0 +1,36 @@
+// src/api/api.js
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const signup = async (username, password) => {
+    const response = await fetch(`${API_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    return await response.json();
+};
+
+export const login = async (username, password) => {
+    const response = await fetch(`${API_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+    }
+
+    return await response.json();
+};
