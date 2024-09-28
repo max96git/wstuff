1d3578c1e1e80fefaf10c4902636f12488e1c4b6
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-28 17:56:59 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home'; // Create this component
-import SellLimiteds from './components/SellLimiteds'; // Create this component
-import SellAccounts from './components/SellAccounts'; // Create this component
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Home from './components/Home';
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/sell-limiteds" component={SellLimiteds} />
-        <Route path="/sell-accounts" component={SellAccounts} />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/login" component={Login} />
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
