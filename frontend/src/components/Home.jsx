c25c6802eb7dcb712fd37fa3f8bd2342443e880e
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-29 15:36:31 +0300

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SellLimiteds from './SellLimiteds';
 import SellAccounts from './SellAccounts';
+import Navbar from './Navbar';
 
 const Home = () => {
   const [items, setItems] = useState([]);
