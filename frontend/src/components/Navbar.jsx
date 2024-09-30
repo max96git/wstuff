a05cd7fa1e9d5c10bf0734cc5faf6a1dd978cb8b
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-30 16:13:56 +0300

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,15 @@
-// src/components/Navbar.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="navbar-logo">
-                <img src="https://tr.rbxcdn.com/2d3b1ae7c0a4d640fca2510826d7d0b4/420/420/Hat/Png" alt="Hexanoid Logo" />
-                <span>Hexanoid</span>
-            </div>
-            <div className="navbar-links">
-                <Link to="/">Home</Link>
-                <Link to="/sell-limiteds">Sell Limiteds</Link>
-                <Link to="/sell-accounts">Sell Accounts</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Signup</Link>
-            </div>
+            <h1>Hexanoid</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/sell-limiteds">Sell Limiteds</a></li>
+                <li><a href="/sell-accounts">Sell Accounts</a></li>
+            </ul>
         </nav>
     );
 };
