b41cf9308d06f4e99497ef7356fb5027793f7131
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-30 16:33:13 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home'; // home
-import Navbar from './components/Navbar'; // navbar
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 const App = () => {
     return (
         <Router>
