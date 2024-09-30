6d6b701795a05faccddf047b2a89ee109e579d54
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-30 21:16:51 +0300

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import '../styles/Navbar.css';
+import { Link } from 'react-router-dom';
+import './Navbar.css';  // Modern CSS for Navbar
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1>Hexanoid</h1>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/sell-limiteds">Sell Limiteds</a></li>
-                <li><a href="/sell-accounts">Sell Accounts</a></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">Hexanoid</Link>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/limiteds">Limiteds</Link></li>
+        <li><Link to="/accounts">Accounts</Link></li>
+        <li><Link to="/sell">Sell</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
