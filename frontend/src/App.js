85a687b35e92afa90f338328fee0ac78b2eb6ed0
[ B ] <119216428+max96git@users.noreply.github.com>
2024-09-30 21:00:44 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
-const App = () => {
-    return (
-        <Router>
-            <div>
-                <Navbar />
-                <Switch>
-                    <Route path="/" element={<Home />} />
-                    {/* others.. */}
-                </Switch>
-            </div>
-        </Router>
-    );
-};
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
